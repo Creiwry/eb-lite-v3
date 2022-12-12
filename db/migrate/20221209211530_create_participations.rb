aed6cb7f9e508b61e7215915e0961ff7a589f9e0
Creiwry <jay.gardos@outlook.com>
2022-12-12 18:07:58 +0100

    feat(model): add belongs_to relations to participations

--- db/migrate/20221209211530_create_participations.rb
+++ db/migrate/20221209211530_create_participations.rb
@@ -2,6 +2,8 @@ class CreateParticipations < ActiveRecord::Migration[7.0]
   def change
     create_table :participations do |t|
       t.string :stripe_customer_id
+      t.belongs_to :event, index: true, foreign_key: true
+      t.belongs_to :participant, index: true, foreign_key: true
 
       t.timestamps
     end
