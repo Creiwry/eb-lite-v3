fa3b981b84ee47b3d25b7284815ba389422a7542
Creiwry <jay.gardos@outlook.com>
2022-12-12 18:51:08 +0100

    fix(model): fix event model typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   has_many :participations
-  has_many :participants, class_name: 'User', through: participations
+  has_many :participants, class_name: 'User', through: :participations
   belongs_to :user
 end
