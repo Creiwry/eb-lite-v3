eb6806d29bf0b6eec4a05bae8b4cc027eb618e5d
Creiwry <jay.gardos@outlook.com>
2022-12-15 18:02:53 +0100

    chore(test): write basic rails tests for event

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
+require 'faker'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @event = Event.new(
+      start_date: Faker::Time.forward,
+      duration: 150,
+      title: 'My Chemical Romance - Swarm Tour',
+      description: 'MCR returns with a new album on this world-spanning tour, so get your eye-liner and hair-straighteners at the ready, the mayor of emo town is making a comeback.',
+      price: 25,
+      location: 'Paris',
+      organiser: User.all.sample
+    )
+  end
+
+  test 'event should be valid' do
+    assert @event.valid?
+  end
+
+  test 'start_date should not be in the past' do
+    @event.start_date = '20220310' 
+    assert_not @event.valid?
+  end
+
+  test
+
 end
