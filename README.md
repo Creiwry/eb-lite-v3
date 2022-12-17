02ee36c6ec03f65fdacb8108c104de9ecadfa98b
Creiwry <jay.gardos@outlook.com>
2022-12-17 16:08:52 +0100

    tests(specs): add tests for welcome_email and join_event_email for participation and user

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+- emails don't send
+- write tests for mailer
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,57 @@
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe UserMailer, type: :mailer do
-  
-end
+# RSpec.describe UserMailer, type: :mailer do
+
+#   describe 'welcome email' do
+#     @user = User.new(
+#       first_name: 'Creiwry',
+#       last_name: 'Iero',
+#       email: 'creiwry.iero@yopmail.com',
+#       encrypted_password: 'secret'
+#     )
+#     let(:mail) { UserMailer.welcome_email(@user).deliver_now }
+
+#     it 'renders the subject' do
+#       expect(mail.subject).to eq('Welcome to eventbrite-lite!')
+#     end
+
+#     it 'renders the receiver email' do
+#       expect(mail.to).to eq([@user.email])
+#     end
+
+#     it 'renders the sender email' do
+#       expect(mail.from).to eq(['no-reply@eblite.com'])
+#     end
+
+#     it 'assigns @name' do
+#       expect(mail.body.encoded).to match(@user.first_name)
+#     end
+#   end
+
+#   describe 'join event email' do
+#     @user = User.last
+#     @event = Event.last
+#     @participation = Participation.new(
+#       participant: @user,
+#       event: @event,
+#       stripe_customer_id: 'stilldontknowwhatthisshouldbe'
+#     )
+#     @mail = UserMailer.join_event_email(@participation).deliver_now
+
+#     it 'renders the subject' do
+#       expect(@mail.subject).to eq('You have joined an event!')
+#     end
+
+#     it 'renders the receiver email' do
+#       expect(@mail.to).to eq([@user.email])
+#     end
+
+#     it 'renders the sender email' do
+#       expect(@mail.from).to eq(['no-reply@eblite.com'])
+#     end
+
+#     it 'assigns @name' do
+#       expect(@mail.body.encoded).to match(@user.first_name)
+#     end
+#   end
+# end
