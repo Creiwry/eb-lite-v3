6dd5d1668ed183631ea140d976318339e494ba5c
Creiwry <jay.gardos@outlook.com>
2022-12-18 19:23:04 +0100

    feat(views): add static_pages secret and index views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '07d0f60ef5dda5c35935d18b14167545ebc0429fe15f18cf010f88eb6dee9cdcdffa057f7866cffd1fcb64bcb9954f0c7f1d3bcf3e0436346042ce823d3f3614'
-
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/secret'
+  root 'static_pages#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
